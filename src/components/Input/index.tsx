7e509a87a36c2a02e648ffe7c71ca1185169d0ac
Wil.i.am <wiliam.tae@gmail.com>
2020-08-16 14:47:08 -0300

    input on focus and blur handled

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,11 @@
-import React, { useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
+import React, {
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+  useState,
+  useCallback,
+} from 'react';
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
 
@@ -26,11 +33,25 @@ const Input: React.RefForwardingComponent<inputRef, InputProps> = (
   const { registerField, defaultValue = '', fieldName, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
 
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputValueRef.current.value);
+  }, [])
+
   useImperativeHandle(ref, () => ({
     focus() {
       inputElementRef.current.focus();
     }
    }))
+
   useEffect(() => {
     registerField<string>({
       name: fieldName,
@@ -48,16 +69,21 @@ const Input: React.RefForwardingComponent<inputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360"/>
+    <Container
+      isFocused={isFocused}
+    >
+      <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000': '#666360'}/>
       <TextInput
         ref={inputElementRef}
         keyboardAppearance="dark"
-        placeholderTextColor="#666360" {...rest}
+        placeholderTextColor="#666360"
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         defaultValue={defaultValue}
         onChangeText={(value) => {
           inputValueRef.current.value = value;
         }}
+        {...rest}
       />
     </Container>
   )
